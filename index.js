b2a57106874c57af0376ad1fe7e4f804aa1370c5
Awa Diop <nadiop37@gmail.com>
2021-12-22 16:24:00 +0000

    ajout du fiche de renseignement des apprenants

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
+// recuperation du formulaire
+var myform = document.getElementsByClassName('formulaire')
+// recuperation des info du formulair
+var inputNom = document.getElementsByClassName('name')
+var inputPrenom = document.getElementsByClassName('prenom')
+var inputBio = document.getElementsByClassName('bio')
+var option = document.getElementsByClassName('')
+var niveau = option.value
+// recuperation du bouton ajouter
+var boutonAjouter = document.getElementsByClassName('btnAdd')
 
+
+
+// tableau d'objet
+var liste_apprenant =[
+    {
+        nom:"Eva",
+        prenom: "diop",
+        niveau: "debutant",
+        bio: "je suis entrains dessayer un truc",
+        photo: 'header.JPG'
+    },
+    {
+        nom:"zalle",
+        prenom: "talla",
+        niveau: "debutant",
+        bio: " A peur de manger son made",
+        photo:'header.JPG'
+    }
+]
+function creerProfil (){
+
+}
+// verification des données
+boutonAjouter.addEventListener('click', ()=> {
+    if (inputNom ){
+        alert('Veuillez renseigner les champs!')
+    }
+}) 
