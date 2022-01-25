98380b7220dbe1b87dda58bacc600ba3d9425bb7
Awa Diop <nadiop37@gmail.com>
2022-01-25 17:55:35 +0000

    ajout des données dans supabase

--- src/app.js
+++ src/app.js
@@ -1,17 +1,23 @@
-import { applyRouting } from "./routing.js";
 
 
 const SUPABASE_URL = 'https://uapervjhiibhkhzcpbfq.supabase.co/rest/v1/Apprenant?'
 const SUPABASE_KEY =
                     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MDI3NDAwMywiZXhwIjoxOTU1ODUwMDAzfQ.ifXAy-0sSyQq91FOkkHOv9ruD1NZ8p_mUZWxVXB7NkQ"
-const allItems =[
-    {
-        "nom": "",
-        "prenom": "",
-        "bio": "",
-        "niveau": "",
-        "photo": null
-    }
+const Apprenant_items =[
+    // {
+    //     nom:"Eva",
+    //     prenom: "diop",
+    //     niveau: "debutant",
+    //     bio: "je suis entrains dessayer un truc",
+    //     photo: 'header.JPG'
+    // },
+    // {
+    //     nom:"zalle",
+    //     prenom: "talla",
+    //     niveau: "debutant",
+    //     bio: " A peur de manger son made",
+    //     photo:'header.JPG'
+    // }
 ]
 const ajouterApp = (item) =>{ 
     // on selectionne le containeur 
@@ -33,69 +39,114 @@ const ajouterApp = (item) =>{
                         </div>
                         </div>  
                         <div class="col-md-2">
-                            <i class="bi bi-pencil btn" style="color: green;"></i>
-                            <i class="bi bi-x-lg btn" style="color: #ce0033;"></i>
+                            <i class="bi bi-pencil btn" style="color: green;"  ></i>
+                            <i class="bi bi-x-lg btn" style="color: #ce0033;" ></i>
                         </div>
                     </div>
                 </div>
             </div>
         
     `)
-    // localStorage.setItem(key, value);
+    
 }
-
+// Apprenant_items.forEach((item) => ajouterApp(item));
 
 // On souhaite réagir à chaque fois que le formulaire est soumis
 document.querySelector('.formulaire').addEventListener("submit",(event)=>{
-    // On souhaite aussi empêcher le rechargement de la page
+//     // On souhaite aussi empêcher le rechargement de la page
     event.preventDefault()
-    // On récupère les inputs
-    console.log('avant iput recup');
     const inputNom = document.getElementById('nom').value
     const inputPrenom = document.getElementById('prenom').value
     const select = document.getElementById('selected').selectedOptions[0].value
     const bio = document.getElementById('biographie').value
 
- //   On créé un nouveau profil d'apprenant
     const item = {
         nom:inputNom,
         prenom: inputPrenom,
         niveau: select,
-        bio: bio,
-        // photo: avatar
-    }
+        bio: bio
+        }
+        ajouterApp(item)
+    
+    const sauvegarderbtn = document.getElementById('sauvegarder')
+    sauvegarderbtn.addEventListener('click',() =>{
 
-    fetch(SUPABASE_URL, {
-            method: "POST",
-            body: JSON.stringify(item),
-            headers: {
-                "Content-Type": "application/json",
-                apiKey: SUPABASE_KEY,
-            Prefer: "return=representation",
-            },
-        })
-        .then((response) => response.json())
-        .then((items) => {
-            ajouterApp(items[0]);
-        });
         
-        window.addEventListener("DOMContentLoaded", (event) => {
-            //RECUPERATION DES DONNEES VIA API
-            fetch(SUPABASE_URL, {
-            method: "GET",
-            headers: {
-                apikey: SUPABASE_KEY,
-            },
-            }).then((response) => response.json())
-            .then((items) => {
-                allItems.forEach((item) => {
-                    ajouterApp(item)
-                    
-                })
-            })
-        })
+    fetch(SUPABASE_URL, {
+        method: "POST",
+        body: JSON.stringify(item),
+        headers: {
+            "Content-Type": "application/json",
+            apiKey: SUPABASE_KEY,
+        Prefer: "return=representation",
+        },
+    })
+    .then((response) => response.json())
+    .then((items) => {
+        ajouterApp(items[0]);
+    }); 
+
+    })
+    
+// Lorsque les éléments du DOM sont tous connus
+document.addEventListener("DOMContentLoaded", () => {
+    // Appel HTTP vers Supabase
     // On vide l'input et replace le curseur dedans
     document.querySelector(".formulaire").reset()
+  // recuperation du btn sauvegarder
+    
+});
+
+// recuperation du btnliste des apprenants
+const btnListApp = document.getElementsByClassName('listeApp');
+console.log(btnListApp)
+
+
+//     // On récupère les inputs
+//     console.log('avant iput recup');
+//     const inputNom = document.getElementById('nom').value
+//     const inputPrenom = document.getElementById('prenom').value
+//     const select = document.getElementById('selected').selectedOptions[0].value
+//     const bio = document.getElementById('biographie').value
+
+//  //   On créé un nouveau profil d'apprenant
+//     const item = {
+//         nom:inputNom,
+//         prenom: inputPrenom,
+//         niveau: select,
+//         bio: bio
+//     }
+
+//     fetch(SUPABASE_URL, {
+//             method: "POST",
+//             body: JSON.stringify(item),
+//             headers: {
+//                 "Content-Type": "application/json",
+//                 apiKey: SUPABASE_KEY,
+//             Prefer: "return=representation",
+//             },
+//         })
+//         .then((response) => response.json())
+//         .then((items) => {
+//             ajouterApp(items[0]);
+//         });
+        
+//         window.addEventListener("DOMContentLoaded", (event) => {
+//             //RECUPERATION DES DONNEES VIA API
+//             fetch(SUPABASE_URL, {
+//             method: "GET",
+//             headers: {
+//                 apikey: SUPABASE_KEY,
+//             },
+//             }).then((response) => response.json())
+//             .then((items) => {
+//                 allItems.forEach((item) => {
+//                     ajouterApp(item)
+                    
+//                 })
+//             })
+//         })
+//     // On vide l'input et replace le curseur dedans
+//     document.querySelector(".formulaire").reset()
 
 }) 
-
